19ebef79ef158ec60d154ad95d29b1428fde7c49
CG-SKYLN <70344561+CG-SKYLN@users.noreply.github.com>
2021-05-26 13:19:07 -0600

    Add files via upload

---
+++ src/Supercar Stats v1.0/bugatti.supercarstatsdevelopment.epizy.com/htdocs/data.js
@@ -0,0 +1,59 @@
+var cars = [
+  "Divo",
+  "Chiron",
+  "Veyron"
+];
+
+var images = [
+  "images/Bugatti-Divo.png",
+  "images/Bugatti-Chiron.png",
+  "images/Bugatti-Veyron-16.4.png"
+];
+
+var prices = [
+  "$5.4 million",
+  "$2.9 million",
+  "$1.9 million"
+];
+
+var speeds = [
+  "236 mph",
+  "304 mph",
+  "267 mph"
+];
+
+var accelTimes = [
+  "2.4 sec.",
+  "2.3 sec.",
+  "2.5 sec."
+];
+
+var qtrMile = [
+  "9.4 sec.",
+  "9.4 sec.",
+  "9.4 sec."
+];
+
+var horsepower = [
+  "1,459 bhp",
+  "1,479 bhp",
+  "1,184 bhp"
+];
+
+var weights = [
+  "2.16 tons (4,323 lbs.)",
+  "2.18 tons (4,359 lbs.)",
+  "2.02 tons (4,044 lbs.)"
+];
+
+var facts = [
+  "As is with many of Bugatti's newest models, the design is slightly similar to one of the earlier types of cars Bugatti made.",
+  "A special version of the Chiron Super Sport 300+ model broke a world record for land speed in a production automobile in 2019.",
+  "One special model of the Veyron was the 16.4 Super Sport. It set a world record for land speed in a production automobile in 2010."
+];
+
+var modelYear = [
+  "2020",
+  "2018",
+  "2012"
+];
---
+++ src/Supercar Stats v1.0/lamborghini.supercarstatsdevelopment.epizy.com/htdocs/data.js
@@ -0,0 +1,59 @@
+var cars = [
+  "Huracan",
+  "Aventador",
+  "Countach"
+];
+
+var images = [
+  "images/Lamborghini-Huracan.png",
+  "images/Lamborghini-Aventador.png",
+  "images/Lamborghini-Countach.png"
+];
+
+var prices = [
+  "$208,000",
+  "$500,000",
+  "$300,000"
+];
+
+var speeds = [
+  "202 mph",
+  "217 mph",
+  "185 mph"
+];
+
+var accelTimes = [
+  "2.9 sec.",
+  "2.9 sec.",
+  "5.3 sec."
+];
+
+var qtrMile = [
+  "10.9. sec.",
+  "10.3 sec.",
+  "13.5 sec."
+];
+
+var horsepower = [
+  "621 bhp",
+  "749 bhp",
+  "434 bhp"
+];
+
+var weights = [
+  "1.57 tons (3,135 lbs.)",
+  "1.81 tons (3,615 lbs.)",
+  "1.32 tons (2,646 lbs.)"
+];
+
+var facts = [
+  "The Italian police force use special models of the Lamborghini Huracan as patrol cars. There is a compartment in the front that can be used to hold organs needing to be transported quickly.",
+  "Similar to te Huracan, the Aventador has many different versions, ranging from the SVJ, a performance focused style, to the roofless Aventador J with a suprising amount of power packed into it.",
+  "The name comes from a swear word, exclaimed by the head of the Bertone design house when seeing the car's design for the first time."
+];
+
+var modelYear = [
+  "2014",
+  "2011",
+  "1974"
+];
