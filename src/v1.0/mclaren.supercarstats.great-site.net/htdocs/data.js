03c67e6e002bf5cc26b2e9433fa03c773d9f9ecb
CG-SKYLN <70344561+CG-SKYLN@users.noreply.github.com>
2021-05-27 09:51:01 -0600

    Add files via upload

---
+++ src/v1.0/mclaren.supercarstats.great-site.net/htdocs/data.js
@@ -0,0 +1,59 @@
+var cars = [
+  "P1",
+  "Speedtail",
+  "Spider"
+];
+
+var images = [
+  "images/McLaren-P1.png",
+  "images/McLaren-Speedtail.png",
+  "images/McLaren-Spider.png"
+];
+
+var prices = [
+  "$1.5 million",
+  "$1.4 million",
+  "$208,800"
+];
+
+var speeds = [
+  "217 mph",
+  "250 mph",
+  "204 mph"
+];
+
+var accelTimes = [
+  "2.6 sec.",
+  "2.9 sec.",
+  "3.1 sec."
+];
+
+var qtrMile = [
+  "9.8 sec.",
+  "To Be Announced",
+  "10.3 sec."
+];
+
+var horsepower = [
+  "903 bhp",
+  "1,036 bhp",
+  "570 bhp"
+];
+
+var weights = [
+  "1.54 tons (3,075 lbs.)",
+  "1.58 tons (3,153 lbs.)",
+  "1.50 tons (3,016 lbs.)"
+];
+
+var facts = [
+  "There were 21 prototypes of the P1 sold before official production began. There are so many different customizable configurations of the P1 that McLaren offers that no two P1s have been built the same.",
+  "There is a certian driving mode called Velocity mode, which is activated by a button in the roof of the cabin. This mode must be used in order to achieve the 250 mph top speed.",
+  "The name 'Spider' is given to roofless or convertable McLaren model. This specific model shown and analyzed is the 720S. Other models include the 600LT, GT, and the concept model 570S."
+];
+
+var modelYear = [
+  "2013",
+  "2018",
+  "2020"
+];
